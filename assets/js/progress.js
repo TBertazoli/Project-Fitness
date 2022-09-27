5bcd3e6ee6611808624ab3769d3950bfeb84b3bd
TB <tatianabertazoli@gmail.com>
2022-09-26 22:54:47 -0500

    added BMI function

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -7,8 +7,9 @@ var todaysDate = moment().format("YYYY-MM-DD");
 var mainSection = $("#mainsection");
 var apiKey = "c45e4b59343fbe91045af5de68b5208ef9091deb";
 var storageData = null;
-var weightInput = $("#weight")
-var heightInput = $("#height")
+var weightInput = $("#weight");
+var heightInput = $("#height");
+var BMI = 0;
 
 
 //Progress function called onclick button
@@ -19,6 +20,7 @@ var showProgress = function () {
 }
 
 function populateWeightHistory() {
+    $('#showweightbody').empty();
     if (!storageData.weight || storageData.weight.length === 0) {
         return;
     }
@@ -43,12 +45,13 @@ function showHideWeightModal(show) {
 //function to save weight
 function submitWeight() {
     storageData.weight = storageData.weight || [];
-    storageData.weight.push({
+    storageData.weight.unshift({
         date: todaysDate,
         weight: weightInput.val()
-    });   
+    });
     localStorage.setItem("BBB", JSON.stringify(storageData));
     showHideWeightModal(false);
+    populateWeightHistory();
 }
 
 //function to show or hide height modal
@@ -64,39 +67,48 @@ function showHideHeightModal(show) {
 //function to save Height
 function submitHeight() {
     storageData.height = storageData.height || [];
-    storageData.height.push({
+    storageData.height.unshift({
         date: todaysDate,
         height: heightInput.val()
     });
-        localStorage.setItem("BBB", JSON.stringify(storageData));
+    localStorage.setItem("BBB", JSON.stringify(storageData));
     showHideHeightModal(false);
 }
-
-
-
-
-
-
-
-
 
 function readFromStorage() {
     storageData = JSON.parse(localStorage.getItem("BBB")) || {};
     if (storageData.events) {
-        storageData.events.forEach(e=>{
+        storageData.events.forEach(e => {
             e.date = moment(e.date);
         });
         console.log(storageData.events);
     }
 }
 
-function addWorkoutHistory () {
+function addWorkoutHistory() {
 
 }
 
 
 
-// function calculateBMI(){
-//     var BMI = weightInput /(heightdisplay.exp(2))
-//     console.log(BMI);
-// }
+function calculateBMI() {
+    if (storageData.height && storageData.height.length > 0 &&
+        storageData.weight && storageData.weight.length > 0) {
+        var weight = storageData.weight[0].weight; // in pounds
+        var height = storageData.height[0].height; // in inches
+        BMI = (weight / 2.205) / ((height / 39.37) * 2);
+        var text = ''
+        if (BMI < 18.5) {
+            text = "Your BMI falls within the underweight range";
+        } else if ((BMI > 18.5) && (BMI < 24.9)) {
+            text = "Your BMI falls within the normal or healthy weight range";
+        } else if ((BMI > 25) && (BMI < 29.9)) {
+            text = "Your BMI falls within the overweight range";
+        } else {
+            text = "Your BMI falls within the obese range";
+        }
+
+        $("#BMI-text").text(`BMI = ${BMI}, ${text}`);
+        $("#alert-border-3").removeClass("hidden opacity-0");
+    }
+};
