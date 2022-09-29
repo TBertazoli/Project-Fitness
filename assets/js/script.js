7ef31465b9e56a49f7995a82153952dfcc137ded
TyFB <tfb1223@gmail.com>
2022-09-29 16:46:13 -0500

    getting input

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,17 @@
 var displayExercise = function () {
     var exerciseAPI = "https://wger.de/api/v2/exercise/?language=2"
     fetch(exerciseAPI).then(function (response) {
-        console.log(exerciseAPI);
         response.json().then(function (data) {
             console.log(data);
-            for (i = 0; i < data.results.length; i++) 
+            for (i = 0; i < data.results.length; i++)
                 $(".exercise").append('<h2 class="workout">' + data.results[i].name + '</h2>');
-                if ($('.workout').click(function(e){
-                    e.preventDefault();
-                    $(".workout").append(data.results[i].description);
-                }));
-                
+                $(".workout").append('<form>Date: <input type="text" id="datepicker"></form>');
+                $(function() {$("#datepicker").datepicker();});    
+                if ($('.workout').click(function (e) {
+                e.preventDefault();
+                $(".workout").append(data.results[i].description);
+                console.log(data.results[i].description);
+            }));
         });
     });
 };
