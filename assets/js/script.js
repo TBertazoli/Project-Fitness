44a92219a0cb9920eed723ca421e6e8c9222c29e
TyFB <tfb1223@gmail.com>
2022-09-29 23:33:14 -0500

    done for tonight

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+$(document).ready(() => {
+    displayExercise();
+});
 
 // gives the list of exercises
 var displayExercise = function () {
@@ -6,13 +9,14 @@ var displayExercise = function () {
         response.json().then(function (data) {
             console.log(data);
             for (i = 0; i < data.results.length; i++)
+                //var array = data.results[i];
                 $(".exercise").append('<h2 class="workout">' + data.results[i].name + '</h2>');
             $(".workout").append('<button class="btn">Completed</button>');
             if ($('.workout').click(function () {
                 //e.preventDefault();
-                $(".workout").append(data.results.description);
+                //$(".workout").append(data.results[i].description);
+                //console.log(data.results[i].description);
             }));
         });
     });
 };
-displayExercise();
